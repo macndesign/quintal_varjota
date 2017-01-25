79a2902562f3d974d6176350112263065d941df3
Mário Chaves <macndesign@gmail.com>
2017-01-24 22:03:09 -0300

    fix configs 7

--- quintal_varjota/settings.py
+++ quintal_varjota/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Email configuration
 
