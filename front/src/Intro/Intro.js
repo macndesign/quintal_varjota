3c743d2f28af1e4934bef65040f9d39a410da1fd
Mário Chaves <macndesign@gmail.com>
2017-07-21 10:14:03 -0300

    main renamed

--- front/src/Intro/Intro.js
+++ front/src/Intro/Intro.js
@@ -12,7 +12,8 @@ class Intro extends Component {
     return (
       <div id="intro" className="intro">
         <div className="video-wrapper">
-          <video loop="true" muted="true" autoPlay="true" poster="img/videoframe.jpg">
+          {/*<video loop="true" muted="true" autoPlay="true" poster="img/videoframe.jpg">*/}
+          <video loop="true" muted="true" autoPlay="true">
             <source src={videoMP4} type="video/mp4"/>
             {/*<source src="video/big_buck_bunny.ogv" type="video/ogg">
             <source src="video/big_buck_bunny.webm" type="video/webm">*/}
